1196a4a1f48e1db4604b5689bd3a659918ef6192
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-27 23:29:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Success from "./components/Success";
+import Users from "./components/users/Users";
+import './index.scss';
+
 
 function App() {
   return (
     <div className="App">
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
